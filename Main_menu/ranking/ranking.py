34b08dfd2e8cf9bcf0929bd506e81efffb89efb3
Matías De Marco <demarcomatias25@gmail.com>
2024-11-15 21:00:59 -0300

    coments

--- Main_menu/ranking/ranking.py
+++ Main_menu/ranking/ranking.py
@@ -9,25 +9,37 @@ def users():
                 users = json.load(file)
             except json.JSONDecodeError:
                 users = {}
-        return users
+        return users #Se abre r el archivo users, solo necesitamos leer este archivo para ver los datos de los usuarios.
     else:
         print("No hay jugadores registrados.")
-        return {}
+        return {} #Si no hay usuarios... para que continuar?
 
 def ranking_wins():
-    user_data = users()
+    # Obtiene los datos de los usuarios llamando a la función 'users()'
+    user_data = users() 
+    # Si no hay datos de usuarios, la función termina.
     if not user_data:
         return
+    # Ordena los usuarios por la cantidad de partidas ganadas en orden descendente
     user_order = sorted(user_data.items(), key=lambda item: item[1]['partidas_ganadas'], reverse=True)
+    # Imprime el título del ranking
     print("Ranking de partidas ganadas:")
+    # Itera sobre la lista de usuarios ordenados, asignando la posición y descomponiendo en 'username' y 'data'
     for position, (username, data) in enumerate(user_order, start=1):
-        print(f"{position} - {username} - {data['partidas_ganadas']}")
+        # Imprime la posición, nombre de usuario y partidas ganadas
+        print(f"{position} - {username} - {data['partidas_ganadas']}") 
 
 def ranking_plays():
+    # Obtiene los datos de los usuarios llamando a la función 'users()'
     user_data = users()
+    # Si no hay datos de usuarios, la función termina.
     if not user_data:
         return
+    # Ordena los usuarios por la cantidad de partidas jugadas en orden descendente
     user_order = sorted(user_data.items(), key=lambda item: item[1]['partidas_jugadas'], reverse=True)
+    # Imprime el título del ranking
     print("Ranking de partidas jugadas:")
+    # Itera sobre la lista de usuarios ordenados, asignando la posición y descomponiendo en 'username' y 'data'
     for position, (username, data) in enumerate(user_order, start=1):
+        # Imprime la posición, nombre de usuario y partidas jugadas
         print(f"{position} - {username} - {data['partidas_jugadas']}")
